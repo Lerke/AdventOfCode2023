9c032966486831728b0c97a06df243746798ae0d
Willem <willemsporen@gmail.com>
2023-12-05 23:41:05 +0100

    Start day 5

---
+++ Day05.IfYouGiveASeedAFertilizer/Program.fs
@@ -0,0 +1,65 @@
+﻿open System
+open System.IO
+open System.IO.MemoryMappedFiles
+open System.Text.RegularExpressions
+
+type Almenac =
+    { SeedsToPlant: int list
+      Sections: AlmenacSection list }
+
+and AlmenacLine =
+    { DestinationRangeStart: int
+      SourceRangeStart: int
+      RangeLength: int }
+
+and AlmenacSection =
+    { SourceName: string
+      TargetName: string
+      Lines: AlmenacLine list }
+
+let ParseAlmenacFile file =
+    let seedMapRegex = Regex("seeds:.*")
+
+    let sourceToDestinationMapRegex =
+        Regex(@"\w+-to-\w+ map:.*?(?:^$|\Z)", RegexOptions.Singleline ||| RegexOptions.Multiline)
+
+    let input = File.ReadAllText file
+
+    let seedsInput =
+        seedMapRegex.Match(input).Value.Split(":")[1]
+        |> fun f -> Regex.Matches(f, "\d+")
+        |> Seq.map (fun f -> f.Value |> int)
+
+    let maps =
+        sourceToDestinationMapRegex.Matches(input)
+        |> Seq.map (fun f ->
+            let lines = f.Value.Split Environment.NewLine
+
+            let (source, target) =
+                (lines[0].Split("-")[0], lines[0].Split("-").[2].Split(" ")[0])
+
+            let mappingLines =
+                lines
+                |> Seq.skip 1
+                |> Seq.filter (fun f -> String.IsNullOrWhiteSpace(f) = false)
+                |> Seq.map (fun f ->
+                    match f.Split(" ") with
+                    | [| destinationStart; sourceStart; rangeLength |] ->
+                        { DestinationRangeStart = destinationStart |> int
+                          SourceRangeStart = sourceStart |> int
+                          RangeLength = rangeLength |> int })
+
+            { SourceName = source
+              TargetName = target
+              Lines = mappingLines |> Seq.toList })
+
+    maps
+
+match Environment.GetCommandLineArgs() with
+| [| _; file |] ->
+    let almenac = ParseAlmenacFile file
+    printfn "%A" almenac
+    0 |> ignore
+| _ ->
+    printf "Usage: dotnet run /path/to/file"
+    1 |> ignore
--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 My own solutions to the Advent of Code 2022 problems.
 
-| Day | Language | Progress        | Solution                                                                                       |
-|-----|----------|-----------------|------------------------------------------------------------------------------------------------|
-| 1   | F#       | :star2: :star2: | [Day01.Trebuchet](https://github.com/Lerke/AdventOfCode2023/tree/main/Day01.Trebuchet)         |
-| 2   | F#       | :star2: :star2: | [Day02.CubeConundrum](https://github.com/Lerke/AdventOfCode2023/tree/main/Day02.CubeConundrum) |
-| 3   | F#       | :star2: :star2: | [Day03.GearRatios](https://github.com/Lerke/AdventOfCode2023/tree/main/Day03.GearRatios)       |
-| 4   | F#       | :star2: :star2: | [Day04.Scratchcards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day04.Scratchcards)     |
+| Day | Language | Progress       | Solution                                                                                                  |
+|-----|----------|----------------|-----------------------------------------------------------------------------------------------------------|
+| 1   | F#       | :star2: :star2: | [Day01.Trebuchet](https://github.com/Lerke/AdventOfCode2023/tree/main/Day01.Trebuchet)                    |
+| 2   | F#       | :star2: :star2: | [Day02.CubeConundrum](https://github.com/Lerke/AdventOfCode2023/tree/main/Day02.CubeConundrum)            |
+| 3   | F#       | :star2: :star2: | [Day03.GearRatios](https://github.com/Lerke/AdventOfCode2023/tree/main/Day03.GearRatios)                  |
+| 4   | F#       | :star2: :star2: | [Day04.Scratchcards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day04.Scratchcards)              |
+| 5   | F#       |                | [Day05.IfYouGiveASeedAFertilizer](https://github.com/Lerke/AdventOfCode2023/tree/main/Day05.IfYouGiveASeedAFertilizer) |
