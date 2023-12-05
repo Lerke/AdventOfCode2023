747bd75e3f54655b04ca0fe4542574891e0531fa
Willem <willemsporen@gmail.com>
2023-12-05 21:48:06 +0100

    Day 4 **

--- Day04.Scratchcards/Program.fs
+++ Day04.Scratchcards/Program.fs
@@ -1,17 +1,44 @@
 ﻿open System
+open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
-type ScratchCard = { Id: int; WinningNumbers: int array; CardNumbers: int array }
+type ScratchCard =
+    { Id: int
+      WinningNumbers: int array
+      CardNumbers: int array }
 
-let rec ScratchCardPointValue (card: ScratchCard) =
+
+let ScratchCardMatchingNumbers (card: ScratchCard) =
     Set.intersect (card.CardNumbers |> Set.ofArray) (card.WinningNumbers |> Set.ofArray)
     |> Set.count
+
+let rec ScratchCardPointValue (card: ScratchCard) =
+    card
+    |> ScratchCardMatchingNumbers
     |> fun f ->
         match f with
         | 0 -> 0
         | n -> Math.Pow(2, (n - 1) |> float) |> int
 
+let rec SolvePartTwo (cardCounts: Map<int, (int * ScratchCard)>) (index: int) =
+    match cardCounts.ContainsKey index with
+    | false -> cardCounts
+    | true ->
+        let currentCard = cardCounts[index]
+        let mutable cards = cardCounts
+        let currentCardValue = ScratchCardMatchingNumbers(snd currentCard)
+        let multiplier = (fst currentCard)
+
+        match (currentCardValue > 0) with
+        | true ->
+            [| (index + 1) .. (index + currentCardValue) |]
+            |> Array.iter (fun f ->
+                let el = cardCounts[f]
+                cards <- cards |> Map.remove f |> Map.add f (((fst el) + multiplier), snd el))
+
+            SolvePartTwo cards (index + 1)
+        | false -> SolvePartTwo cards (index + 1)
 
 let rec ParseLine line =
     match Regex.Match(line, @"(Card\s+(?<card_id>\d+)): (?<numbers>.*)") with
@@ -27,7 +54,8 @@ let rec ParseLine line =
             |> Seq.toArray }
     | _ -> failwithf $"Could not parse input line %s{line}"
 
-let ParseFile file = file |> File.ReadAllLines |> Seq.map ParseLine
+let ParseFile file =
+    file |> File.ReadAllLines |> Seq.map ParseLine
 
 match Environment.GetCommandLineArgs() with
 | [| _; file |] ->
@@ -36,6 +64,17 @@ match Environment.GetCommandLineArgs() with
     let totalPointsForCards = pointsPerCard |> Seq.sum
 
     printfn $"[*]: Total point worth of all cards: %i{totalPointsForCards}"
+
+    let groupedCards =
+        cards
+        |> Seq.groupBy (fun f -> f.Id)
+        |> Seq.map (fun f -> ((fst f), (1, (snd f |> Seq.head))))
+        |> Map
+
+    let totalCards = SolvePartTwo groupedCards 1
+    let totalCardCount = totalCards.Values |> Seq.sumBy fst
+
+    printfn $"[**]: Total cards %i{totalCardCount}"
     0 |> ignore
 | _ ->
     printf "Usage: dotnet run /path/to/file"
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ My own solutions to the Advent of Code 2022 problems.
 | 1   | F#       | :star2: :star2: | [Day01.Trebuchet](https://github.com/Lerke/AdventOfCode2023/tree/main/Day01.Trebuchet)         |
 | 2   | F#       | :star2: :star2: | [Day02.CubeConundrum](https://github.com/Lerke/AdventOfCode2023/tree/main/Day02.CubeConundrum) |
 | 3   | F#       | :star2: :star2: | [Day03.GearRatios](https://github.com/Lerke/AdventOfCode2023/tree/main/Day03.GearRatios)       |
-| 4   | F#       | :star2:         | [Day04.Scratchcards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day04.Scratchcards)     |
+| 4   | F#       | :star2: :star2: | [Day04.Scratchcards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day04.Scratchcards)     |
