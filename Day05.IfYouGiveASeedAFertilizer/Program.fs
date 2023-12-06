ff0cdc292e90765ac29cd89bbdb47f8903851e3b
Willem <willemsporen@gmail.com>
2023-12-06 20:22:14 +0100

    Day 5 *

--- Day05.IfYouGiveASeedAFertilizer/Program.fs
+++ Day05.IfYouGiveASeedAFertilizer/Program.fs
@@ -1,27 +1,38 @@
 ﻿open System
 open System.IO
-open System.IO.MemoryMappedFiles
 open System.Text.RegularExpressions
 
-type Almenac = { SeedsToPlant: int list; Sections: AlmenacSection list }
+type Almenac =
+    { SeedsToPlant: int64 list
+      Sections: AlmenacSection list }
 
-and AlmenacLine = { DestinationRangeStart: int; SourceRangeStart: int; RangeLength: int }
+and AlmenacLine =
+    { DestinationRangeStart: int64
+      SourceRangeStart: int64
+      RangeLength: int64 }
 
-and AlmenacSection = { SourceName: string; TargetName: string; Lines: AlmenacLine list }
+and AlmenacSection =
+    { SourceName: string
+      TargetName: string
+      Lines: AlmenacLine list }
 
-type AlmenacSeedSoilMapping = Map<int, int>
+type AlmenacSeedSoilMapping = Map<int64, int64>
 
-let GetMappingForSeedNumber (almenac: Almenac) (target: string) (seed: int) =
-    almenac.Sections
-    |> Seq.find (fun f -> f.TargetName = target)
+let GetMappingForAlmenacSectionAndSeedNumber (section: AlmenacSection) (seed: int64) =
+    section
     |> (fun f ->
-            (f.Lines
-             |> List.tryFind (fun p -> p.SourceRangeStart <= seed && seed <= (p.SourceRangeStart + p.RangeLength))))
+        (f.Lines
+         |> List.tryFind (fun p -> p.SourceRangeStart <= seed && seed <= (p.SourceRangeStart + p.RangeLength))))
     |> fun f ->
         match f with
-        | Some z ->
-            z |> fun f -> (f.DestinationRangeStart) + (seed - f.SourceRangeStart)
+        | Some z -> z |> fun f -> (f.DestinationRangeStart) + (seed - f.SourceRangeStart)
         | None -> seed
+
+let GetMappingForSeedNumber (almenac: Almenac) (seed: int64) =
+    almenac.Sections
+    |> fun f ->
+        f
+        |> List.fold (fun acc ele -> GetMappingForAlmenacSectionAndSeedNumber ele acc) seed
 
 let ParseAlmenacFile file =
     let seedMapRegex = Regex("seeds:.*")
@@ -34,12 +45,12 @@ let ParseAlmenacFile file =
     let seedsInput =
         seedMapRegex.Match(input).Value.Split(":")[1]
         |> fun f -> Regex.Matches(f, "\d+")
-        |> Seq.map (fun f -> f.Value |> int)
+        |> Seq.map (fun f -> f.Value |> int64)
 
     let maps =
         sourceToDestinationMapRegex.Matches(input)
         |> Seq.map (fun f ->
-            let lines = f.Value.Split ("\r\n")
+            let lines = f.Value.Replace("\r\n", "\n").Split("\n")
 
             let (source, target) =
                 (lines[0].Split("-")[0], lines[0].Split("-").[2].Split(" ")[0])
@@ -51,35 +62,29 @@ let ParseAlmenacFile file =
                 |> Seq.map (fun f ->
                     match f.Split(" ") with
                     | [| destinationStart; sourceStart; rangeLength |] ->
-                        { DestinationRangeStart = destinationStart |> int
-                          SourceRangeStart = sourceStart |> int
-                          RangeLength = rangeLength |> int }
+                        { DestinationRangeStart = destinationStart |> int64
+                          SourceRangeStart = sourceStart |> int64
+                          RangeLength = rangeLength |> int64 }
                     | _ -> failwithf $"Unable to parse: {f}")
 
-            { SourceName = source; TargetName = target; Lines = mappingLines |> Seq.toList })
+            { SourceName = source
+              TargetName = target
+              Lines = mappingLines |> Seq.toList })
 
-    { SeedsToPlant = seedsInput |> Seq.toList; Sections = maps |> Seq.toList }
+    { SeedsToPlant = seedsInput |> Seq.toList
+      Sections = maps |> Seq.toList }
 
 match Environment.GetCommandLineArgs() with
 | [| _; file |] ->
     let almenac = ParseAlmenacFile file
 
-    let test =
-        { SeedsToPlant = []
-          Sections =
-            [ { SourceName = "seed"
-                TargetName = "soil"
-                Lines =
-                  [ { DestinationRangeStart = 50; SourceRangeStart = 98; RangeLength = 2 }
-                    { DestinationRangeStart = 52; SourceRangeStart = 50; RangeLength = 48 } ] } ] }
-
-    let locationMapping = GetMappingForSeedNumber almenac "location"
+    let locationNumbers =
+        almenac.SeedsToPlant |> List.map (GetMappingForSeedNumber almenac)
+    let lowestLocationNumber = locationNumbers |> List.min
     
-    let bx = locationMapping 79
-    assert ((locationMapping 79) = 82)
-    assert ((locationMapping 14) = 43)
-    assert ((locationMapping 55) = 86)
-    assert ((locationMapping 13) = 35)
+    printf $"[*] Lowest location number: %i{lowestLocationNumber}"
+
+
     0 |> ignore
 | _ ->
     printf "Usage: dotnet run /path/to/file"
