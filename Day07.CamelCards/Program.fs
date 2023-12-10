a1a39e06b365274fdcb2a00703527c4ebed6da09
Willem <willemsporen@gmail.com>
2023-12-10 20:55:22 +0100

    Day 7 *

--- Day07.CamelCards/Program.fs
+++ Day07.CamelCards/Program.fs
@@ -3,65 +3,58 @@ open System.IO
 open System.Text.RegularExpressions
 
 type HandType =
-    | FiveOfAKind
-    | FourOfAKind
-    | FullHouse
-    | ThreeOfAKind
-    | TwoPair
-    | OnePair
-    | HighCard
-
-let HandTypeValue handType =
-    match handType with
-    | FiveOfAKind -> 7
-    | FourOfAKind -> 6
-    | FullHouse -> 5
-    | ThreeOfAKind -> 4
-    | TwoPair -> 3
-    | OnePair -> 2
-    | HighCard -> 1
+    | FiveOfAKind = 7
+    | FourOfAKind = 6
+    | FullHouse = 5
+    | ThreeOfAKind = 4
+    | TwoPair = 3
+    | OnePair = 2
+    | HighCard = 1
 
 type PlayingCard =
-    | Ace
-    | King
-    | Queen
-    | Jack
-    | Ten
-    | Nine
-    | Eight
-    | Seven
-    | Six
-    | Five
-    | Four
-    | Three
-    | Two
-    | One
+    | Ace = 14
+    | King = 13
+    | Queen = 12
+    | Jack = 11
+    | Ten = 10
+    | Nine = 9
+    | Eight = 8
+    | Seven = 7
+    | Six = 6
+    | Five = 5
+    | Four = 4
+    | Three = 3
+    | Two = 2
+    | One = 1
 
 let PlayingCard (c: char) =
     match c with
-    | 'A' -> Ace
-    | 'K' -> King
-    | 'Q' -> Queen
-    | 'J' -> Jack
-    | 'T' -> Ten
-    | '9' -> Nine
-    | '8' -> Eight
-    | '7' -> Seven
-    | '6' -> Six
-    | '5' -> Five
-    | '4' -> Four
-    | '3' -> Three
-    | '2' -> Two
-    | '1' -> One
+    | 'A' -> PlayingCard.Ace
+    | 'K' -> PlayingCard.King
+    | 'Q' -> PlayingCard.Queen
+    | 'J' -> PlayingCard.Jack
+    | 'T' -> PlayingCard.Ten
+    | '9' -> PlayingCard.Nine
+    | '8' -> PlayingCard.Eight
+    | '7' -> PlayingCard.Seven
+    | '6' -> PlayingCard.Six
+    | '5' -> PlayingCard.Five
+    | '4' -> PlayingCard.Four
+    | '3' -> PlayingCard.Three
+    | '2' -> PlayingCard.Two
+    | '1' -> PlayingCard.One
 
-type CamelCardsHand = { Id: int; Cards: PlayingCard[]; Bid: int }
+type CamelCardsHand =
+    { Id: int
+      Cards: PlayingCard[]
+      Bid: int64 }
 
 let DetermineHandType (hand: CamelCardsHand) =
     let isFiveOfAKind hand =
         hand.Cards |> Seq.groupBy id |> Seq.length = 1
         |> fun f ->
             match f with
-            | true -> Some FiveOfAKind
+            | true -> Some HandType.FiveOfAKind
             | _ -> None
 
     let isFourOfAKind hand =
@@ -70,7 +63,7 @@ let DetermineHandType (hand: CamelCardsHand) =
         |> (fun f -> (f |> Seq.length) = 2 && (f |> Seq.exists (fun e -> (snd e) |> Seq.length = 4)))
         |> fun f ->
             match f with
-            | true -> Some FourOfAKind
+            | true -> Some HandType.FourOfAKind
             | _ -> None
 
     let isFullHouse hand =
@@ -79,7 +72,7 @@ let DetermineHandType (hand: CamelCardsHand) =
         |> (fun f -> (f |> Seq.length) = 2 && (f |> Seq.exists (fun e -> (snd e) |> Seq.length = 3)))
         |> fun f ->
             match f with
-            | true -> Some FullHouse
+            | true -> Some HandType.FullHouse
             | _ -> None
 
     let isThreeOfAKind hand =
@@ -88,7 +81,7 @@ let DetermineHandType (hand: CamelCardsHand) =
         |> fun f -> (f |> Seq.length = 3) && (f |> Seq.exists (fun e -> (snd e) |> Seq.length = 3))
         |> fun f ->
             match f with
-            | true -> Some ThreeOfAKind
+            | true -> Some HandType.ThreeOfAKind
             | _ -> None
 
     let isTwoPair hand =
@@ -99,7 +92,7 @@ let DetermineHandType (hand: CamelCardsHand) =
             && (f |> Seq.filter (fun e -> (snd e) |> Seq.length = 2) |> Seq.length) = 2
         |> fun f ->
             match f with
-            | true -> Some TwoPair
+            | true -> Some HandType.TwoPair
             | _ -> None
 
     let isOnePair hand =
@@ -108,7 +101,7 @@ let DetermineHandType (hand: CamelCardsHand) =
         |> (fun f -> (f |> Seq.length = 4))
         |> fun f ->
             match f with
-            | true -> Some TwoPair
+            | true -> Some HandType.OnePair
             | _ -> None
 
     let isHighCard hand =
@@ -117,7 +110,7 @@ let DetermineHandType (hand: CamelCardsHand) =
         |> (fun f -> (f |> Seq.length = 5))
         |> fun f ->
             match f with
-            | true -> Some TwoPair
+            | true -> Some HandType.HighCard
             | _ -> None
 
     seq {
@@ -135,6 +128,24 @@ let DetermineHandType (hand: CamelCardsHand) =
         | _ -> false)
     |> fun f -> (f hand).Value
 
+let CompareHands (a: CamelCardsHand) (b: CamelCardsHand) =
+    let aValue = DetermineHandType a
+    let bValue = DetermineHandType b
+
+    match aValue < bValue with
+    | true -> (b, a)
+    | false ->
+        match aValue > bValue with
+        | true -> (a, b)
+        | false ->
+            a.Cards
+            |> Array.zip b.Cards
+            |> Array.find (fun (aCard, bCard) -> aCard <> bCard)
+            |> fun (bCard, aCard) ->
+                match aCard < bCard with
+                | true -> (b, a)
+                | false -> (a, b)
+
 
 let ParseHand (line: string) index =
     Regex.Match(line, "(?<hand>[^\s]+)\s(?<bid>\d+)")
@@ -142,18 +153,28 @@ let ParseHand (line: string) index =
         match f.Groups |> Seq.toArray with
         | [| _; cards; bid |] ->
             { Id = index
-              Bid = bid.Value |> int
-              Cards = cards.Value |> Seq.map PlayingCard |> Seq.toArray }
-        | groups -> failwith $"Could not parse: %s{f.Value}")
+              Bid = bid.Value |> int64
+              Cards = cards.Value |> Seq.map PlayingCard |> Seq.toArray })
 
-let ParseInput file = File.ReadAllLines file |> Seq.mapi (fun i f -> ParseHand f i) |> Seq.toArray
+let ParseInput file =
+    File.ReadAllLines file |> Seq.mapi (fun i f -> ParseHand f i) |> Seq.toArray
 
 match Environment.GetCommandLineArgs() with
 | [| _; file |] ->
     let hands = ParseInput file
-    let types = hands |> Seq.map DetermineHandType |> Seq.toArray
-    printfn "%A" hands
-    printfn "%A" types
+
+    let totalWinnings =
+        hands
+        |> Array.sortWith (fun a b ->
+            match CompareHands a b with
+            | (x, _) when x.Id = a.Id -> -1
+            | (x, _) when x.Id = b.Id -> 1
+            | _ -> 0)
+        |> Array.rev
+        |> Array.zip [| 1 .. hands.Length |]
+        |> Array.fold (fun acc (rank, card) -> acc + ((rank |> int64) * card.Bid)) 0L
+
+    printfn $"[*] Total winnings: %i{totalWinnings}"
     0 |> ignore
 | _ ->
     printf "Usage: dotnet run /path/to/file"
--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 My own solutions to the Advent of Code 2023 problems.
 
-| Day | Language | Progress      | Solution                                                                                                               |
-|-----|----------|---------------|------------------------------------------------------------------------------------------------------------------------|
+| Day | Language | Progress        | Solution                                                                                                               |
+|-----|----------|-----------------|------------------------------------------------------------------------------------------------------------------------|
 | 1   | F#       | :star2: :star2: | [Day01.Trebuchet](https://github.com/Lerke/AdventOfCode2023/tree/main/Day01.Trebuchet)                                 |
 | 2   | F#       | :star2: :star2: | [Day02.CubeConundrum](https://github.com/Lerke/AdventOfCode2023/tree/main/Day02.CubeConundrum)                         |
 | 3   | F#       | :star2: :star2: | [Day03.GearRatios](https://github.com/Lerke/AdventOfCode2023/tree/main/Day03.GearRatios)                               |
 | 4   | F#       | :star2: :star2: | [Day04.Scratchcards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day04.Scratchcards)                           |
 | 5   | F#       | :star2: :star2: | [Day05.IfYouGiveASeedAFertilizer](https://github.com/Lerke/AdventOfCode2023/tree/main/Day05.IfYouGiveASeedAFertilizer) |
 | 6   | F#       | :star2: :star2: | [Day06.WaitForIt](https://github.com/Lerke/AdventOfCode2023/tree/main/Day06.WaitForIt)                                 |
-| 7   | F#       |  | [Day07.CamelCards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day07.CamelCards)                                |
+| 7   | F#       | :star2:         | [Day07.CamelCards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day07.CamelCards)                                |
