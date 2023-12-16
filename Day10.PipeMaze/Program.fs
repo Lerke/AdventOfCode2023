3c3239e48e8b2a6e34ed978a5e0c285e027d51d2
Willem <willem.sporen@scenius.nl>
2023-12-16 17:58:06 +0100

    Start Day 10

---
+++ Day10.PipeMaze/Program.fs
@@ -0,0 +1,130 @@
+﻿open System
+open System.IO
+
+type Map = { Tiles: Tile[,]; StartingPosition: int * int }
+
+and Tile =
+    | VerticalPipe
+    | HorizontalPipe
+    | NorthEastBend
+    | NorthWestBend
+    | SouthWestBend
+    | SouthEastBend
+    | Ground
+    | StartingPosition
+
+let ParseTile (tile: char) =
+    match tile with
+    | '|' -> VerticalPipe
+    | '-' -> HorizontalPipe
+    | 'L' -> NorthEastBend
+    | 'J' -> NorthWestBend
+    | '7' -> SouthWestBend
+    | 'F' -> SouthEastBend
+    | '.' -> Ground
+    | 'S' -> StartingPosition
+    | n -> failwithf $"Unknown input: {n}"
+
+let NextTile (x: int) (y: int) (px: int) (py: int) tile =
+    match tile with
+    | VerticalPipe -> if py < y then (x, y + 1) else (x, y - 1)
+    | HorizontalPipe -> if (px < x) then (x + 1, y) else (x - 1, y)
+    | NorthEastBend -> if (py < y) then (x + 1, y) else (x, y - 1)
+    | NorthWestBend -> if (py < y) then (x - 1, y) else (x, y - 1)
+    | SouthWestBend -> if (py > y) then (x - 1, y) else (x, y + 1)
+    | SouthEastBend -> if (py > y) then (x + 1, y) else (x, y + 1)
+    | Ground -> failwith "we should never navigate over ground!"
+    | StartingPosition -> failwith "Starting position should not be part of main loop!"
+
+let SelectTile x y (tiles: Tile[,]) =
+    if y < 0 || y >= tiles.[*, 0].Length then Ground
+    elif x < 0 || x >= tiles.[y, *].Length then Ground
+    else tiles.[y, x]
+
+let ConnectingPositions (x: int) (y: int) tiles =
+    match (SelectTile x y tiles) with
+    | VerticalPipe -> x, y - 1
+    | HorizontalPipe -> (x + 1), y
+    | NorthEastBend -> x, y - 1
+    | NorthWestBend -> x, y - 1
+    | SouthWestBend -> x, y + 1
+    | SouthEastBend -> x, y + 1
+    | _ -> failwithf "This tile has no connecting positions"
+
+let rec FollowPipe (map: Map) (position: int * int) (nextPosition: int * int) (visited: (int * int) list) =
+    printf $"{snd position}, {fst position}"
+    match (visited |> List.contains position) with
+    | true -> visited
+    | false ->
+        let py, px = position
+        let y, x = nextPosition
+        printf " -> "
+        let nextTile = NextTile x y px py (SelectTile x y map.Tiles)
+        printfn $"{x}, {y}"
+        FollowPipe map (y, x) nextTile (visited @ [ position ])
+
+let ParseMapFile file =
+    let rec startPosition x y (tiles: Tile[,]) =
+        if x >= tiles[0,*].Length then startPosition 0 (y + 1) tiles
+        elif tiles.[y, x] = StartingPosition then (x, y)
+        else startPosition (x + 1) y tiles
+
+    let determineStartingTile x y tiles =
+        let leftTile = SelectTile (x - 1) y tiles
+        let rightTile = SelectTile (x + 1) y tiles
+        let topTile = SelectTile x (y - 1) tiles
+        let bottomTile = SelectTile x (y + 1) tiles
+
+        let topConnects =
+            [| VerticalPipe; SouthEastBend; SouthWestBend |] |> Array.contains topTile
+
+        let bottomConnects =
+            [| VerticalPipe; NorthEastBend; NorthWestBend |] |> Array.contains bottomTile
+
+        let rightConnects =
+            [| HorizontalPipe; SouthWestBend; NorthWestBend |] |> Array.contains rightTile
+
+        let leftConnects =
+            [| HorizontalPipe; NorthEastBend; SouthEastBend |] |> Array.contains leftTile
+
+        match (topConnects, bottomConnects, rightConnects, leftConnects) with
+        | true, true, _, _ -> VerticalPipe
+        | true, _, true, _ -> NorthEastBend
+        | true, _, _, true -> NorthWestBend
+        | _, true, true, _ -> SouthEastBend
+        | _, true, _, true -> SouthWestBend
+        | _, _, true, true -> HorizontalPipe
+        | _ -> failwith "Failed to determine starting tile"
+
+    let tiles =
+        file |> File.ReadLines |> Seq.map (fun f -> f |> Seq.map ParseTile) |> array2D
+
+    let startingPosition = startPosition 0 0 tiles
+
+    let startingTile =
+        determineStartingTile (fst startingPosition) (snd startingPosition) tiles
+
+    tiles[(fst startingPosition), (snd startingPosition)] <- startingTile
+
+    let map = { Tiles = tiles; StartingPosition = startingPosition }
+    map
+
+match Environment.GetCommandLineArgs() with
+| [| _; file |] ->
+    let map = ParseMapFile file
+
+    let nextPosition =
+        ConnectingPositions (fst map.StartingPosition) (snd map.StartingPosition) map.Tiles
+
+    let path = FollowPipe map map.StartingPosition nextPosition []
+
+    let pathLengths =
+        path |> List.mapi (fun i f -> {| Tile = f; Steps = (min i (path.Length - i)) |})
+
+    let furthestPoint = pathLengths |> List.maxBy (_.Steps)
+
+    printfn $"[*] Steps along the loop to point farthest from starting position: {furthestPoint.Steps}"
+    0 |> ignore
+| _ ->
+    printf "Usage: dotnet run /path/to/file"
+    1 |> ignore
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 My own solutions to the Advent of Code 2023 problems.
 
-| Day | Language | Progress        | Solution                                                                                                               |
-|-----|----------|-----------------|------------------------------------------------------------------------------------------------------------------------|
+| Day | Language | Progress       | Solution                                                                                                               |
+|-----|----------|----------------|------------------------------------------------------------------------------------------------------------------------|
 | 1   | F#       | :star2: :star2: | [Day01.Trebuchet](https://github.com/Lerke/AdventOfCode2023/tree/main/Day01.Trebuchet)                                 |
 | 2   | F#       | :star2: :star2: | [Day02.CubeConundrum](https://github.com/Lerke/AdventOfCode2023/tree/main/Day02.CubeConundrum)                         |
 | 3   | F#       | :star2: :star2: | [Day03.GearRatios](https://github.com/Lerke/AdventOfCode2023/tree/main/Day03.GearRatios)                               |
@@ -13,3 +13,4 @@ My own solutions to the Advent of Code 2023 problems.
 | 7   | F#       | :star2: :star2: | [Day07.CamelCards](https://github.com/Lerke/AdventOfCode2023/tree/main/Day07.CamelCards)                               |
 | 8   | F#       | :star2: :star2: | [Day08.HauntedWasteland](https://github.com/Lerke/AdventOfCode2023/tree/main/Day08.HauntedWasteland)                   |
 | 9   | F#       | :star2: :star2: | [Day09.MirageMaintenance](https://github.com/Lerke/AdventOfCode2023/tree/main/Day09.MirageMaintenance)                 |
+| 10  | F#       |                | [Day10.PipeMaze](https://github.com/Lerke/AdventOfCode2023/tree/main/Day10.PipeMaze)                                   |
